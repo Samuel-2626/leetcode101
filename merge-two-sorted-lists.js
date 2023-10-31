3411ec3a0e18a1327b365662fa28567cb438140d
Samuel Torimiro <efosonaname@gmail.com>
2023-10-30 19:12:09 -0500

    merge two sorted lists

--- merge-two-sorted-lists.js
+++ merge-two-sorted-lists.js
@@ -13,20 +13,29 @@ class ListNode {
 
 function mergeTwoLists(list1, list2) {
 
-    if (!list1 && !list2) {
-        return
-    } 
-    
-    if (!list1) {
-        return list2
-    } 
-    
-    if (!list2) {
-        return list1
-    } 
+    let dummy = new ListNode(0)
+    let head = dummy
+
+    while (list1 && list2) {
+        if (list1.value <= list2.value) {
+            dummy.next = list1
+            list1 = list1.next
+        } else {
+            dummy.next = list2 
+            list2 =  list2.next
+        }
+        dummy = dummy.next
+    }
+
+    if (list1 !== null) {
+        dummy.next = list1
+    } else {
+        dummy.next = list2
+    }
+
+    return head.next
 
  
-
 }
 
 const rootNodeA = new ListNode(1)
@@ -36,7 +45,6 @@ const nodeC = new ListNode(4)
 rootNodeA.next = nodeB
 nodeB.next = nodeC
 
-// console.log(rootNodeA);
 
 const rootNodeB = new ListNode(1)
 const nodeD = new ListNode(3)
@@ -45,8 +53,7 @@ const nodeE = new ListNode(4)
 rootNodeB.next = nodeD
 nodeD.next = nodeE
 
-// console.log(rootNodeB);
-
 
 
 console.log(mergeTwoLists(rootNodeA, rootNodeB));
+
